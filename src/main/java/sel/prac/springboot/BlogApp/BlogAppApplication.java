81d2fc05cffebd4b94e9f5557c8eb0d597882c31
KHAN <selim.deep.25@gmail.com>
2023-12-04 15:11:03 +0530

    basic app

---
+++ src/main/java/sel/prac/springboot/BlogApp/BlogAppApplication.java
@@ -0,0 +1,13 @@
+package sel.prac.springboot.BlogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sel/prac/springboot/BlogApp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sel.prac.springboot.BlogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
